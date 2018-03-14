ac39085e404c1634fa24897b6b60f2c21bc93de7
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-13 23:18:27 -0700

    Add get and set methods for name property

--- src/Model/Entity/EntityType.php
+++ src/Model/Entity/EntityType.php
@@ -6,10 +6,22 @@ use LeoGalleguillos\Entity\Model\Entity as EntityEntity;
 class EntityType
 {
     protected $entityTypeId;
+    protected $name;
 
     public function getEntityTypeId() : int
     {
         return $this->entityTypeId;
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name) : EntityEntity\EntityType
+    {
+        $this->name = $name;
+        return $this;
     }
 
     public function setEntityTypeId(int $entityTypeId) : EntityEntity\EntityType
