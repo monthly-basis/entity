dde4d04a72516ae2286f8f4683c9bc801812f717
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-10-04 23:32:36 +0000

    Modernize table model unit tests

--- test/Model/Table/EntityTypeTest.php
+++ test/Model/Table/EntityTypeTest.php
@@ -4,41 +4,20 @@ namespace LeoGalleguillos\EntityTest\Model\Table;
 use ArrayObject;
 use Exception;
 use LeoGalleguillos\Entity\Model\Table as EntityTable;
-use LeoGalleguillos\EntityTest\TableTestCase;
+use LeoGalleguillos\Test\TableTestCase;
 use Zend\Db\Adapter\Adapter;
 use PHPUnit\Framework\TestCase;
 
 class EntityTypeTest extends TableTestCase
 {
-    /**
-     * @var string
-     */
-    protected $sqlPath = __DIR__ . '/../../..' . '/sql/leogalle_test/entity_type/';
-
     protected function setUp()
     {
-        $configArray     = require(__DIR__ . '/../../../config/autoload/local.php');
-        $configArray     = $configArray['db']['adapters']['leogalle_test'];
-        $this->adapter   = new Adapter($configArray);
-
-        $this->entityTypeTable = new EntityTable\EntityType($this->adapter);
+        $this->entityTypeTable = new EntityTable\EntityType($this->getAdapter());
 
         $this->setForeignKeyChecks0();
-        $this->dropTable();
-        $this->createTable();
+        $this->dropTable('entity_type');
+        $this->createTable('entity_type');
         $this->setForeignKeyChecks1();
-    }
-
-    protected function dropTable()
-    {
-        $sql = file_get_contents($this->sqlPath . 'drop.sql');
-        $result = $this->adapter->query($sql)->execute();
-    }
-
-    protected function createTable()
-    {
-        $sql = file_get_contents($this->sqlPath . 'create.sql');
-        $result = $this->adapter->query($sql)->execute();
     }
 
     public function testInitialize()
